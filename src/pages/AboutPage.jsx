da08a0bfc1965e093dc91e91b936541c5fa16129
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-16 13:36:43 +0000

    Add routes v.6 and About page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import Card from "../components/shared/Card"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Card>
+        <h1>About this project</h1>
+        <p>This is a React App to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <Link to="/">Back to home</Link>
+        </p>
+      </Card>
+    </div>
+  )
+}
+
+export default AboutPage

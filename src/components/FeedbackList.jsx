bdd458370f66e36914dcdc5efa18d8b717432b55
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-08 12:18:26 +0000

    Add Loading... message when feedback is loading

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,20 +4,19 @@ import FeedbackContext from '../context/FeedbackContext'
 import FeedbackItem from "./FeedbackItem"
 
 const FeedbackList = () => {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if(!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>
   }
-
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem
-        key={item.id}
-        item={item}/>
-      ))}
-    </div>
+  return isLoading ? ( <h3>Loading...</h3> ) : (
+   <div className="feedback-list">
+    {feedback.map((item) => (
+      <FeedbackItem
+      key={item.id}
+      item={item}/>
+    ))}
+  </div>
   )
 }
 
